67e5dd253b45dc7b49ce9fe9dcdb0f426bd40aac
Sachin Kelkar <smk2256@columbia.edu>
2018-10-25 14:59:18 -0400

    Add initial version of app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# coms_4156
+# coms_4156 ![travisci](https://travis-ci.com/s4chin/coms_4156.svg?token=3tkeMeV6pyEjAXfzc7xZ&branch=master)
 Course project for ASE
---
+++ models.py
@@ -0,0 +1,20 @@
+import datetime
+import os
+
+from peewee import *
+
+path = os.getenv('HOME', os.path.expanduser('~')) + '/.notes'
+db = SqliteDatabase(path + '/diary.db')
+
+
+class Note(Model):
+    """
+    Note model in db
+    """
+    title = CharField()
+    content = TextField()
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    tags = CharField(null=True)
+
+    class Meta:
+        database = db
