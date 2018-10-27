2dfe5175c9b80983d088ad82f8302c3196c418e6
Aviral Gupta <aviral@dyn-160-39-149-79.dyn.columbia.edu>
2018-10-27 15:26:12 -0400

    Add Note Unit Test Added

--- test_notes.py
+++ test_notes.py
@@ -1,6 +1,21 @@
 # Demo file of tests
-from notes import fn
+from notes import fn, add_entry 
+from models import Note
 
 
 def test_fn():
     assert fn(4) == 4**2
+
+def test_add_entry():
+	title = "Hello World"
+	content = "How are you doing today?"
+	add_entry(content, title)
+	entry = Note.get(Note.title == title)
+	print(entry)
+	assert entry.content == content
+
+	
+
+
+
+
