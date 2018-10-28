3e42e78bb662d0cb3a5aa259a6ee683bc48ca95f
Sachin Kelkar <smk2256@columbia.edu>
2018-10-28 19:04:16 -0400

    Fix tests again

--- test_notes.py
+++ test_notes.py
@@ -1,5 +1,5 @@
 # Demo file of tests
-from notes import fn, add_entry, delete_entry, edit_entry 
+from notes import fn, add_entry, delete_entry, edit_entry
 from peewee import *
 import models as m
 from crypto_utils import encrypt, key_to_store
@@ -10,13 +10,16 @@ m.proxy.initialize(db_temp)
 db_temp.connect()
 db_temp.create_tables([m.Note], safe=True)
 
+
 def test_add_entry():
-	title = "avi"
-	content = "How are you doing today?"
-	add_entry(content, title)
-	entry = m.Note.get(m.Note.title == title)
-	print(entry)
-	assert entry.content == content
+    title = "avi"
+    content = "How are you doing today?"
+    password = "masterpassword"
+    add_entry(content, title, password)
+    entry = m.Note.get(m.Note.title == title)
+    print(entry)
+    assert entry.content == content
+
 
 def test_delete_entry():
     title = "yo"
@@ -54,24 +57,17 @@ def test_edit_entry():
     assert (entry.title, entry.content, entry.password, flag) == (new_title, encryped_data, password_to_store, 1)
 
 
-
-
-
-
-
-
-	
 '''def test_fn():
     assert fn(4) == 4**2'''
 
 '''class Test(TestCase):
-	def test_add_entry(self):
-		with test_database(test_db, Note):
-			title = "Hello World"
-			content = "How are you doing today?"
-			add_entry(content, title)
-			entry = Note.get(Note.title == title)
-			self.assertEqual(entry.content, content)'''
+    def test_add_entry(self):
+        with test_database(test_db, Note):
+            title = "Hello World"
+            content = "How are you doing today?"
+            add_entry(content, title)
+            entry = Note.get(Note.title == title)
+            self.assertEqual(entry.content, content)'''
 
 
 
