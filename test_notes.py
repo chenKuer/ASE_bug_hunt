57962c2e67814b21b1a62b31e47340a1996acb2d
Aviral Gupta <aviral@Avirals-MBP.lan1>
2018-10-27 21:27:50 -0400

    Edit Note Test Added

--- test_notes.py
+++ test_notes.py
@@ -1,5 +1,5 @@
 # Demo file of tests
-from notes import fn, add_entry, delete_entry 
+from notes import fn, add_entry, delete_entry, edit_entry 
 from peewee import *
 import models as m
 
@@ -31,6 +31,21 @@ def test_delete_entry():
     if entry.exists():
         flag2 = 0
     assert flag1 == flag2
+
+def test_edit_entry():
+    title = "lost in this world"
+    content = "Batman is forever lost!!!"
+    m.Note.create(content=content, tags=None, title=title)
+    entry = m.Note.get(m.Note.title == title)
+    new_title = "Superhero Found"
+    new_content = "Robin to the rescue!!!"
+    edit_entry(entry, new_title, new_content)
+    entry = m.Note.select().where(m.Note.title == title)
+    flag = 1
+    if entry.exists():
+        flag = 0
+    entry = m.Note.get(m.Note.title == new_title)
+    assert (entry.title, entry.content, flag) == (new_title, new_content, 1)
 
 
 
